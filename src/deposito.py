5f1b5fb2d6fe0889defdff3f00200cd9d96bc654
ArmandoLuz <armandoari.288@gmail.com>
2022-08-12 18:13:51 -0300

    Deposit class

--- src/deposito.py
+++ src/deposito.py
@@ -1,4 +1,5 @@
 from PyQt5 import uic
+from dataBase import db
 
 class Deposito:
 
@@ -29,22 +30,22 @@ class Deposito:
         self._buttonDep.clicked.connect(lambda: self.deposita())
         self._buttonBackDep.clicked.connect(lambda: self.back())
     
-    def _load_info(self, account):
-        self._account = account
+    def _load_info(self, cpf):
+        self._cpf = cpf
 
     def back(self):
         #Faz o retorno para a dashboard
         self._valor.setText("")
         self._window.labelStatusDep.setText("")
 
-        self._appWindow._dash._load_info(self._account)
+        self._appWindow._dash._load_info(self._cpf)
         self._appWindow.go_to(3)
     
     def deposita(self):
-        if self._valor.text() == '':
-            self._window.labelStatusDep.setText("Digite um valor!")
+        if self._valor.text() == '' or float(self._valor.text()) <= 0:
+            self._window.labelStatusDep.setText("Digite um valor válido!")
             return 0
         else:
-            status = self._account.deposita(float(self._valor.text()))
+            status = db.deposit(self._cpf, float(self._valor.text()))
             self._window.labelStatusDep.setText(status)
             self._window.inpValDep.setText("")
